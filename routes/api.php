4918f18f009d3f499c754de3e023235826351465
pawasxac <fawwazsetiawan02@gmail.com>
2026-05-12 11:06:36 +0700

    Fix API routing: add apiPrefix to separate admin and API komunitas routes

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CoffeeShopController;
 use App\Http\Controllers\Api\KomunitasController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 // Rute API Coffee Shop
 Route::apiResource('coffee-shops', CoffeeShopController::class);
